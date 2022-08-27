d818744213bd6144b3a8ad63f58c0ff4b508efd3
DariaOdessa <dariaonlynew@gmail.com>
2022-08-27 16:01:59 +0300

    add hw-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -43,6 +43,20 @@ export class App extends Component {
     this.setState({ filter: e.currentTarget.value });
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { contacts, filter } = this.state;
     const normalizedFilter = filter.toLowerCase();
